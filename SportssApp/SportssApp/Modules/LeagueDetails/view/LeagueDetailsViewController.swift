2ea7a923a43c6f2f2775e25f5166ba150b7d7db9
sarrahAshrraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-05-22 09:09:12 -0400

    compositional view UI

---
+++ SportssApp/SportssApp/Modules/LeagueDetails/view/LeagueDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LeagueDetailsViewController.swift
+//  SportssApp
+//
+//  Created by sarrah ashraf on 22/05/2024.
+//
+
+import UIKit
+
+class LeagueDetailsViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
