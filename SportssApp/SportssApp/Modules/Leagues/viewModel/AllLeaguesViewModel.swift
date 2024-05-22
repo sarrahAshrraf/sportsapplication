03a6979114427012d0da22471368b07e3cc30339
sarrahAshrraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-05-22 08:52:51 -0400

    done all leagues screen UI And Logic

---
+++ SportssApp/SportssApp/Modules/Leagues/viewModel/AllLeaguesViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  AllLeaguesViewModel.swift
+//  SportssApp
+//
+//  Created by sarrah ashraf on 22/05/2024.
+//
+
+import Foundation
+import Alamofire
+
+class LeagueViewModel{
+    
+var bindResultToViewController : (() -> ()) = {}
+
+var result : [League]? = [] {
+    didSet{
+        bindResultToViewController()
+    }
+}
+
+func getData(sportName : String){
+    let leagueUrl = "https://apiv2.allsportsapi.com/\(sportName)/?met=Leagues&APIkey=\(Constants.apiKey)"
+
+    NetworkManager().fetchDataFromAPI(url: leagueUrl) { [weak self] (result : Response?) in
+        self?.result = result?.result
+    }
+}
+    
+    
+    //https://apiv2.allsportsapi.com/football/?&met=Videos&eventId=86392&APIkey=22ad8dd732a55a3fe4d2f4df34998396b28f2b23f9020add2c4c977342017644
+    
+    func getVedio(sportName: String, eventId: String){
+        let leagueVedio = "https://apiv2.allsportsapi.com/\(sportName)/?&met=Videos&eventId=\(eventId)&APIkey=\(Constants.apiKey)"
+        
+        
+        
+        
+    }
+}
---
+++ SportssApp/SportssApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SportssApp
+//
+//  Created by sarrah ashraf on 22/05/2024.
+//
+
+import Foundation
+class Constants{
+    static let apiKey = "995239db0133e854b94ff543d0f5c1e93a86c6ee8d60df34e502c87e932bbb6d"
+       
+    
+}
