5160cf05718566beff6391b46930b4c2775941d9
sarrahAshrraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-05-25 06:22:56 -0400

    done expectation test

--- SportssApp/SportssApp/Models/League.swift
+++ SportssApp/SportssApp/Models/League.swift
@@ -7,20 +7,29 @@
 
 import Foundation
 
-class League: Codable {
-    let leagueKey: Int
-    let leagueName: String
-    let countryKey: Int?
-    let countryName: String
-    let leagueLogo: String?
-    let countryLogo: String?
+//class League: Codable {
+//    let leagueKey: Int
+//    let leagueName: String
+//    let countryKey: Int?
+//    let countryName: String
+//    let leagueLogo: String?
+//    let countryLogo: String?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case leagueKey = "league_key"
+//        case leagueName = "league_name"
+//        case countryKey = "country_key"
+//        case countryName = "country_name"
+//        case leagueLogo = "league_logo"
+//        case countryLogo = "country_logo"
+//    }
+//}
 
-    enum CodingKeys: String, CodingKey {
-        case leagueKey = "league_key"
-        case leagueName = "league_name"
-        case countryKey = "country_key"
-        case countryName = "country_name"
-        case leagueLogo = "league_logo"
-        case countryLogo = "country_logo"
-    }
+class League : Decodable{
+    var league_key : Int?
+    var league_name : String?
+    var country_key : Int?
+    var country_name : String?
+    var league_logo : String?
+    var country_logo : String?
 }
